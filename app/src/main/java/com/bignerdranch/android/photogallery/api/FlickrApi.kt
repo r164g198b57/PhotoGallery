2e8da3031250f8b65994aac25912ea14a4397acc
r164g198b57 <headerr0ad@gmail.com>
2023-07-04 15:25:52 +0300

    first

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.photogallery.api
+
+import retrofit2.http.GET
+
+private const val API_KEY = "e7253fb2f1148cb18bdf1ebf51e741e4"
+
+interface FlickrApi {
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+            "&api_key=$API_KEY" +
+            "&format=json" +
+            "&nojsoncallback=1" +
+            "&extras=url_s"
+    )
+    suspend fun fetchPhotos(): FlickrResponse
+}
