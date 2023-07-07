1dbc7679b84e8dfc3d41d0d8a0306a8f5e99c567
r164g198b57 <headerr0ad@gmail.com>
2023-07-07 15:50:42 +0300

    fin

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bignerdranch.android.photogallery.databinding.FragmentPhotoPageBinding
@@ -26,10 +29,34 @@ class PhotoPageFragment : Fragment() {
             false
         )
         binding.apply {
+            progressBar.max = 100
+
             webView.apply {
                 settings.javaScriptEnabled = true
                 webViewClient = WebViewClient()
                 loadUrl(args.photoPageUri.toString())
+
+                webChromeClient = object : WebChromeClient() {
+                    override fun onProgressChanged(
+                        webView: WebView,
+                        newProgress: Int
+                    ) {
+                        if (newProgress == 100) {
+                            progressBar.visibility = View.GONE
+                        } else {
+                            progressBar.visibility = View.VISIBLE
+                            progressBar.progress = newProgress
+                        }
+                    }
+
+                    override fun onReceivedTitle(
+                        view: WebView?,
+                        title: String?
+                    ) {
+                        val parent = requireActivity() as AppCompatActivity
+                        parent.supportActionBar?.subtitle = title
+                    }
+                }
             }
         }
         return binding.root
